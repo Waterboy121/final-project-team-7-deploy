54967a115c0b038cac5368cd91590f710722ab06
delandchen <chen.deland@gmail.com>
2023-12-09 21:31:38 -0500

    signup, signin, home page init

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Signin from '../Screens/Signup/signin'
+import Signup from '../Screens/Login/signup'
+import Home from '../Screens/Home/home'
 
-export default function Home() {
+export default function page() {
   return (
     <main className={styles.main}>
-      <p> Test </p>
+      <Home />
     </main>
   )
 }
