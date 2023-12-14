41fb1436022dadbdbc3e45372b4456540e70c119
Caleb <caleb.perez.tdh@gmail.com>
2023-12-14 13:50:10 -0500

    Changed text and icon colors for visibility

--- frontend/src/Screens/Home/home.js
+++ frontend/src/Screens/Home/home.js
@@ -7,17 +7,11 @@ import { HiOutlineHome } from "react-icons/hi";
 import { IoIosPlayCircle, IoIosSettings, IoIosExit } from "react-icons/io";
 import { HiMagnifyingGlass } from "react-icons/hi2";
 
-
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
-				<Image
-					className={styles.logo}
-					src={logo}
-					alt="app logo"
-					priority
-				/>
+				<Image className={styles.logo} src={logo} alt="app logo" priority />
 				<span
 					style={{
 						display: "flex",
@@ -32,7 +26,10 @@ export default function Home() {
 						alt="profile pic"
 						priority
 					/>
-					<p style={{ fontWeight: "bold" }}> Albert Willaims</p>
+					<p style={{ fontWeight: "bold", color: "#FFFFFF" }}>
+						{" "}
+						Albert Willaims
+					</p>
 				</span>
 			</div>
 			<div className={styles.body}>
@@ -42,20 +39,38 @@ export default function Home() {
 						<p className={styles.option}>
 							<HiOutlineHome /> Home
 						</p>
-						<p className={styles.option}> <IoIosPlayCircle /> Playlists </p>
+						<p className={styles.option}>
+							{" "}
+							<IoIosPlayCircle /> Playlists{" "}
+						</p>
 					</div>
 					<div className={styles.toolbarGroup2}>
 						<h3 className={styles.optionHeader}> Other </h3>
-						<p className={styles.option}> <IoIosSettings /> Settings </p>
-						<p className={styles.option} style={{ color: "#FF0000" }}> <IoIosExit /> Log out</p>
+						<p className={styles.option}>
+							{" "}
+							<IoIosSettings /> Settings{" "}
+						</p>
+						<p className={styles.option} style={{ color: "#FF0000" }}>
+							{" "}
+							<IoIosExit /> Log out
+						</p>
 					</div>
 				</div>
-                <div className={styles.main}>
-                    <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                        <p className={styles.icon}> <HiMagnifyingGlass size={"1.5em"}/> </p>
-                        <input className={styles.search} placeholder="Search"/> 
-                    </div> 
-                </div>
+				<div className={styles.main}>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+						}}
+					>
+						<p className={styles.icon}>
+							{" "}
+							<HiMagnifyingGlass size={"1.5em"} color="gray" />{" "}
+						</p>
+						<input className={styles.search} placeholder="Search" />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
