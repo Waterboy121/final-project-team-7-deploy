8696b2e474ad1f08b48f005249a6b62f9433a559
Caleb <caleb.perez.tdh@gmail.com>
2023-12-14 13:30:23 -0500

    changed landing page to login page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Signin from '../Screens/Signin/signin'
-import Signup from '../Screens/Signup/signup'
-import Home from '../Screens/Home/home'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Signin from "../Screens/Signin/signin";
+import Signup from "../Screens/Signup/signup";
+import Home from "../Screens/Home/home";
 
 export default function DefaultPage() {
-  return (
-    <main className={styles.main}>
-      <Signup />
-    </main>
-  )
+	return (
+		<main className={styles.main}>
+			<Signin />
+		</main>
+	);
 }
